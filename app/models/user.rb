a0be5a0911c4ecab7ac2c32fe24d7457a4a7ea70
phillip chung <phillip.chung0@gmail.com>
2016-11-21 19:11:27 -0800

    added family association

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ after_initialize :ensure_session_token
 #
 # has_many :donors, :through => :relationship, :source => :user
 # has_one :family, :through => :relationship, :source => :user
+has_one :family
 
  def password=(password)
    @password = password
