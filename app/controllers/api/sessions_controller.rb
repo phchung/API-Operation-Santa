cdeefb5c6946dbcdb69a8c613bd7483e5a4d52ba
phillip chung <phillip.chung0@gmail.com>
2017-07-02 21:41:45 -0700

    fixed bugs

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -9,16 +9,16 @@ class Api::SessionsController < ApplicationController
       session_obj  = Session.find_by(session_token: session_token)
       if session_obj
         @user = User.find(session_obj.user_id)
+        @session_token = params[:session_token]
       end
     elsif params[:email] && params[:password]
       @user = User.find_by_credentials(params[:email],params[:password])
+      @session_token = generate_token(@user.email)
+      login(@user.id)
     else
       @user = nil
     end
     if @user
-      # @session_token = generate_token(@user.email)
-      # login(@user.id)
-      @session_token = params[:session_token]
       render 'api/users/show'
     else
       if session_obj || params[:session_token]
@@ -36,7 +36,9 @@ class Api::SessionsController < ApplicationController
   def destroy
     @user = current_user
     logout
-    render 'api/users/show'
+    render(
+      json: {"logged_off"=> true}
+      )
   rescue
     render(
       json: {"error"=>"Cannot find user"},status: 401
