9d2a2f28ea3e7d89f4ecf4d46852376d3bb1a326
Carroll Yu <carrollcyu@gmail.com>
2017-07-06 13:47:31 -0700

    secure user delivery data

--- app/controllers/api/postmates_controller.rb
+++ app/controllers/api/postmates_controller.rb
@@ -10,7 +10,7 @@ class Api::PostmatesController < ApplicationController
 
   def get_estimate
     begin
-      user = User.find(params["uid"])
+      user = User.find(params["dropoff_uid"])
       response = RestClient::Request.execute({
                 method: :post,
                 url: 'https://api.postmates.com/v1/customers/'+ "cus_L1UBJKTP5tOgf-" + '/delivery_quotes',
@@ -33,7 +33,7 @@ class Api::PostmatesController < ApplicationController
                 method: :post,
                 url: 'https://api.postmates.com/v1/customers/'+ "cus_L1UBJKTP5tOgf-" + '/deliveries',
                 :headers => { 'Authorization' => "Basic MjBiNDY1MWUtODM1ZC00NmIxLWIwY2YtZmEwZjU2YmU1OTA1Og==" },
-                :payload => deliveries_params.merge(parse_number)})
+                :payload => deliveries_params.merge(setup_delivery_data)})
     rescue => e
       render json: e.http_body, status: 401
     else
@@ -41,20 +41,22 @@ class Api::PostmatesController < ApplicationController
     end
   end
 
-  def parse_number
-    number = {:pickup_phone_number =>
-      params[:pickup_phone_number].gsub(/[^0-9\s]/i, '').delete(" "),
-      :dropoff_phone_number =>
-      params[:dropoff_phone_number].gsub(/[^0-9\s]/i, '').delete(" ")}
-    number
+  def setup_delivery_data
+    user = User.find(params["dropoff_uid"])
+    delivery_data = {
+      :pickup_phone_number => params[:pickup_phone_number].gsub(/[^0-9\s]/i, '').delete(" "),
+      :dropoff_name => user["first_name"] + ' ' + user["last_name"],
+      :dropoff_address => user["address"],
+      :dropoff_phone_number => user["phone_number"]
+    }
+    delivery_data
   end
 
   private
 
   def deliveries_params
     params.permit(
-      :quote_id,:manifest,:dropoff_name,:dropoff_phone_number,:dropoff_address,:pickup_name,:pickup_phone_number,:pickup_address,
-      :robo_delivered,:robo_dropoff,:robo_pickup_complete,:robo_pickup
+      :quote_id,:manifest,:dropoff_name,:dropoff_phone_number,:dropoff_address,:pickup_name,:pickup_phone_number,:pickup_address
     )
   end
 end
