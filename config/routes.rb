cf3c26ccf5c09254c0a293e095314f7a08e5d085
Carroll Yu <carrollcyu@gmail.com>
2017-07-05 21:39:38 -0700

    remove unused access to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create,:destroy]
     resources :user, only: [:create,:index,:show,:update,:destroy]
-    resources :postmates, only: [:index] do
+    resources :postmates, only: [] do
       collection do
         post :get_estimate
         post :create_delivery
       end
     end
   end
-  match '*any' => 'application#options', :via => [:options]
+  # match '*any' => 'application#options', :via => [:options]
 end
